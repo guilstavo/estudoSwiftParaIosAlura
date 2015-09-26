6a962339828c323eb630fe0e51279757a0b3f5fe
Guilstavo <gustavoartinis@gmail.com>
2015-09-26 00:09:51 -0300

    Tipos de váriaveis, Playground, Funções.

---
+++ eggplant-brownie/eggplant-brownie/Base.lproj/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let name = "eggplant"
+let calorias = 50.5
+let eggplantIsVegetable = true
+let happiness = 5
+
+func helloCalories(){
+    print("hello calories!")
+}
+
+helloCalories()
+print("oi tudo bem")
+helloCalories()
+
+func add(name:String, calories:Double){
+    print("adding \(name) \(calories)")
+}
+
+add("eggplant", 50.5)
