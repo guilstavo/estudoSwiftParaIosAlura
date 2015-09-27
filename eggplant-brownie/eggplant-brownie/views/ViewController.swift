066b26ac3f2e0161e3c034ab23f7d9a741d053eb
Guilstavo <gustavoartinis@gmail.com>
2015-09-27 14:52:55 -0300

    Optional chaining e If let

--- eggplant-brownie/eggplant-brownie/views/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/views/ViewController.swift
@@ -40,10 +40,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func add(item: Item) {
         items.append(item)
-        if(tableView == nil){
-            return
+        if let table = tableView{
+            table.reloadData()
+        }else{
+            let alert = UIAlertController(title: "Sorry", message: "Unexpectef error, but the item was added.", preferredStyle: UIAlertControllerStyle.Alert)
+            let ok = UIAlertAction(title: "Understood", style: UIAlertActionStyle.Cancel, handler: nil)
+            alert.addAction(ok)
+            presentViewController(alert, animated: true, completion: nil)
         }
-        tableView!.reloadData()
     }
     
     func showNewItem(){
